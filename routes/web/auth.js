cf68f10062746886b77f2444e6b3157a1ee80227
LiMingCong <2058615288@qq.com>
2026-04-05 23:00:51 +0800

    添加退出登录功能

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -66,4 +66,12 @@ router.post('/login',async (req,res)=>{
     }
 })
 
+// 退出登录
+router.get('/logout',(req,res)=>{
+    // 销毁session
+    req.session.destroy(()=>{
+        res.render('success',{msg:'退出成功',url:'/login'})
+    })
+})
+
 module.exports = router;
